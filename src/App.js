6600d6ce25403c90ad936a6f1b4ff03632d22240
wyteh <wyteh@uw.edu>
2021-10-23 15:45:06 -0700

    font final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-<style>
-@font-face {
-      font-family: keepOnTruckin;
-      src: url(//db.onlinewebfonts.com/c/49fafb302421193937187e7f6957d03b?family=Keep+on+Truckin'FW);
-    }
-</style>
-
 function App() {
   return (
     <div className="App">
