f8d7836288572955ceb2fe03f28e130f99e3df26
wyteh <wyteh@uw.edu>
2021-10-23 15:34:53 -0700

    font

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="topleft">yam</div>
+        <div class="topleft">yam
+        font-family:keepOnTruckin
+        </div>
       </header>
     </div>
   );
+}
+
+@font-face {
+  font-family: keepOnTruckin;
+  src: @import url(//db.onlinewebfonts.com/c/49fafb302421193937187e7f6957d03b?family=Keep+on+Truckin'FW);
 }
 
 export default App;
